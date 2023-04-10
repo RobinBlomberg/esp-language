296e393dab403a2dd51eb9f0ce30b3677035766a
Robin Blomberg <hello@robinblomberg.se>
2023-04-10 18:12:12 +0200

    feat(parser): add call expression support

---
+++ src/productions/left-hand-side-expression.test.ts
@@ -0,0 +1,65 @@
+import { it, suite, test } from 'vitest';
+import { Arguments, CallExpression, Identifier } from '../ast';
+import { createParseAssert } from '../test-utils';
+import { parseLeftHandSideExpression } from './left-hand-side-expression';
+
+const { ok } = createParseAssert(parseLeftHandSideExpression);
+
+suite('LeftHandSideExpression', () => {
+  test('Identifier', () => {
+    ok(' a ', Identifier(1, 2, 'a'));
+  });
+
+  suite('CallExpression', () => {
+    it('should handle non-nested call expressions', () => {
+      ok(
+        'a()',
+        CallExpression(0, 3, Identifier(0, 1, 'a'), Arguments(1, 3, [])),
+      );
+      ok(
+        'a(b)',
+        CallExpression(
+          0,
+          4,
+          Identifier(0, 1, 'a'),
+          Arguments(1, 4, [Identifier(2, 3, 'b')]),
+        ),
+      );
+      ok(
+        'a(b, c)',
+        CallExpression(
+          0,
+          7,
+          Identifier(0, 1, 'a'),
+          Arguments(1, 7, [Identifier(2, 3, 'b'), Identifier(5, 6, 'c')]),
+        ),
+      );
+    });
+
+    it('should handle nested call expressions', () => {
+      ok(
+        'a()()',
+        CallExpression(
+          0,
+          5,
+          CallExpression(0, 3, Identifier(0, 1, 'a'), Arguments(1, 3, [])),
+          Arguments(3, 5, []),
+        ),
+      );
+      ok(
+        'a(b)(c, d)',
+        CallExpression(
+          0,
+          10,
+          CallExpression(
+            0,
+            4,
+            Identifier(0, 1, 'a'),
+            Arguments(1, 4, [Identifier(2, 3, 'b')]),
+          ),
+          Arguments(4, 10, [Identifier(5, 6, 'c'), Identifier(8, 9, 'd')]),
+        ),
+      );
+    });
+  });
+});
---
+++ src/productions/left-hand-side-expression.ts
@@ -0,0 +1,22 @@
+import { CallExpression, Expression } from '../ast';
+import { Parser } from '../parser-utils';
+import { parseArguments } from './arguments';
+import { parseMemberExpression } from './member-expression';
+
+export const parseLeftHandSideExpression: Parser<Expression> = (
+  data,
+  start,
+) => {
+  let callee = parseMemberExpression(data, start);
+  if (!callee) return null;
+
+  while (true) {
+    const arguments_ = parseArguments(data, callee.end);
+    if (arguments_) {
+      callee = CallExpression(callee.start, arguments_.end, callee, arguments_);
+      continue;
+    }
+
+    return callee;
+  }
+};
