e780775c899cd6600e5e86b043f2957a7f55d30d
Robin Blomberg <hello@robinblomberg.se>
2023-04-12 07:01:31 +0200

    feat(parser): add logical AND expression support

---
+++ src/productions/logical-and-expression.test.ts
@@ -0,0 +1,45 @@
+import { describe, it, suite, test } from 'vitest';
+import { BinaryExpression, Identifier } from '../ast';
+import { createParseAssert } from '../test-utils';
+import { parseLogicalANDExpression } from './logical-and-expression';
+
+const { ok } = createParseAssert(parseLogicalANDExpression);
+
+suite('LogicalANDExpression', () => {
+  test('"BitwiseORExpression"', () => {
+    ok(' abc ', Identifier(1, 4, 'abc'));
+  });
+
+  describe('"LogicalANDExpression && BitwiseORExpression"', () => {
+    it('should respect operator precedence', () => {
+      ok(
+        'a && b | c && d | e',
+        BinaryExpression(
+          0,
+          19,
+          '&&',
+          BinaryExpression(
+            0,
+            10,
+            '&&',
+            Identifier(0, 1, 'a'),
+            BinaryExpression(
+              5,
+              10,
+              '|',
+              Identifier(5, 6, 'b'),
+              Identifier(9, 10, 'c'),
+            ),
+          ),
+          BinaryExpression(
+            14,
+            19,
+            '|',
+            Identifier(14, 15, 'd'),
+            Identifier(18, 19, 'e'),
+          ),
+        ),
+      );
+    });
+  });
+});
