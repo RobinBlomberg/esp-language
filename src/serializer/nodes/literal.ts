31a3be9c0c6be0330ae32559ad1b37656004be1c
Robin Blomberg <hello@robinblomberg.se>
2023-04-19 18:12:17 +0200

    test(serializer): add more Literal tests

--- src/serializer/nodes/literal.ts
+++ src/serializer/nodes/literal.ts
@@ -8,9 +8,49 @@ import { Writer } from '../write';
  *   BooleanLiteral
  *   NumericLiteral
  *   StringLiteral
+ *
+ * NullLiteral ::
+ *   null
+ *
+ * BooleanLiteral ::
+ *   true
+ *   false
+ *
+ * NumericLiteral ::
+ *   DecimalLiteral
+ *   DecimalBigIntegerLiteral
+ *   NonDecimalIntegerLiteral[+Sep]
+ *   NonDecimalIntegerLiteral[+Sep] BigIntLiteralSuffix
+ *   LegacyOctalIntegerLiteral
+ *
+ * StringLiteral ::
+ *   " DoubleStringCharacters<opt> "
+ *   ' SingleStringCharacters<opt> '
+ *
+ * DoubleStringCharacters ::
+ *   DoubleStringCharacter DoubleStringCharacters<opt>
+ *
+ * DoubleStringCharacter ::
+ *   SourceCharacter but not one of " or \ or LineTerminator
+ *   <LS>
+ *   <PS>
+ *   \ EscapeSequence
+ *   LineContinuation
+ *
+ * RegularExpressionLiteral ::
+ *   / RegularExpressionBody / RegularExpressionFlags
+ * ```
+ *
+ * ```markdown
+ *
+ * | Code Point | Unicode Name        | Abbreviation |
+ * |------------|---------------------|--------------|
+ * | U+2028     | LINE SEPARATOR      | <LS>         |
+ * | U+2029     | PARAGRAPH SEPARATOR | <PS>         |
  * ```
  *
  * @see https://tc39.es/ecma262/#prod-Literal
+ * @see https://tc39.es/ecma262/#table-line-terminator-code-points
  */
 export const writeLiteral: Writer<Literal> = (node, write) => {
   switch (typeof node.value) {
