02f970701144afe2565c80c108208db10fbbd28c
Robin Blomberg <hello@robinblomberg.se>
2023-04-19 21:19:18 +0200

    feat(serializer): support WhileStatement

--- src/serializer/nodes/while-statement.ts
+++ src/serializer/nodes/while-statement.ts
@@ -1,4 +1,18 @@
 import { WhileStatement } from '../../estree';
 import { Writer } from '../write';
 
-export const writeWhileStatement: Writer<WhileStatement> = (node, write) => {};
+/**
+ * ```ecmarkup
+ * WhileStatement[Yield, Await, Return] :
+ *   while ( Expression[+In, ?Yield, ?Await] ) Statement[?Yield, ?Await, ?Return]
+ * ```
+ *
+ * @see https://tc39.es/ecma262/#prod-WhileStatement
+ */
+export const writeWhileStatement: Writer<WhileStatement> = (node, write) => {
+  write('while');
+  write('(');
+  write(node.test);
+  write(')');
+  write(node.body);
+};
