5c052bcf53259696dbba7a2f721b869a3f6770df
Robin Blomberg <hello@robinblomberg.se>
2023-04-20 17:15:08 +0200

    feat(serializer): support CallExpression

---
+++ src/serializer/nodes/call-expression.test.ts
@@ -0,0 +1,77 @@
+import { expect, suite, test } from 'vitest';
+import { CallExpression, Identifier, SpreadElement } from '../../estree';
+import { serialize } from '../write';
+
+suite('CallExpression', () => {
+  suite('CallExpression[?Yield, ?Await] Arguments[?Yield, ?Await]', () => {
+    suite('Arguments', () => {
+      test('( )', () => {
+        expect(serialize(CallExpression(Identifier('a'), [], false))).toBe(
+          'a()',
+        );
+      });
+
+      suite('( ArgumentList[?Yield, ?Await] )', () => {
+        suite('ArgumentList[?Yield, ?Await]', () => {
+          test('AssignmentExpression[+In, ?Yield, ?Await]', () => {
+            expect(
+              serialize(
+                CallExpression(Identifier('a'), [Identifier('b')], false),
+              ),
+            ).toBe('a(b)');
+          });
+
+          test('... AssignmentExpression[+In, ?Yield, ?Await]', () => {
+            expect(
+              serialize(
+                CallExpression(
+                  Identifier('a'),
+                  [SpreadElement(Identifier('b'))],
+                  false,
+                ),
+              ),
+            ).toBe('a(...b)');
+          });
+
+          test('ArgumentList[?Yield, ?Await] , AssignmentExpression[+In, ?Yield, ?Await]', () => {
+            expect(
+              serialize(
+                CallExpression(
+                  Identifier('a'),
+                  [Identifier('b'), Identifier('c')],
+                  false,
+                ),
+              ),
+            ).toBe('a(b,c)');
+          });
+
+          test(
+            'ArgumentList[?Yield, ?Await] , ' +
+              '... AssignmentExpression[+In, ?Yield, ?Await]',
+            () => {
+              expect(
+                serialize(
+                  CallExpression(
+                    Identifier('a'),
+                    [Identifier('b'), SpreadElement(Identifier('c'))],
+                    false,
+                  ),
+                ),
+              ).toBe('a(b,...c)');
+            },
+          );
+        });
+      });
+    });
+  });
+
+  suite('CallExpression[?Yield, ?Await] OptionalChain[?Yield, ?Await]', () => {
+    suite('OptionalChain[?Yield, ?Await]', () => {
+      test('?. Arguments[?Yield, ?Await]', () => {
+        expect(serialize(CallExpression(Identifier('a'), [], true))).toBe(
+          'a?.()',
+        );
+      });
+    });
+  });
+});
--- src/serializer/nodes/call-expression.ts
+++ src/serializer/nodes/call-expression.ts
@@ -1,13 +1,49 @@
 import { CallExpression } from '../../estree';
 import { Writer } from '../write';
 
+/**
+ * ```ecmarkup
+ * CallExpression[Yield, Await] :
+ *   ...
+ *   CallExpression[?Yield, ?Await] Arguments[?Yield, ?Await]
+ *   ...
+ *
+ * Arguments[Yield, Await] :
+ *   ( )
+ *   ( ArgumentList[?Yield, ?Await] )
+ *   ( ArgumentList[?Yield, ?Await] , )
+ *
+ * ArgumentList[Yield, Await] :
+ *   AssignmentExpression[+In, ?Yield, ?Await]
+ *   ... AssignmentExpression[+In, ?Yield, ?Await]
+ *   ArgumentList[?Yield, ?Await] , AssignmentExpression[+In, ?Yield, ?Await]
+ *   ArgumentList[?Yield, ?Await] , ... AssignmentExpression[+In, ?Yield, ?Await]
+ *
+ * OptionalExpression[Yield, Await] :
+ *   ...
+ *   CallExpression[?Yield, ?Await] OptionalChain[?Yield, ?Await]
+ *   ...
+ *
+ * OptionalChain[Yield, Await] :
+ *   ?. Arguments[?Yield, ?Await]
+ *   ...
+ * ```
+ *
+ * @see https://tc39.es/ecma262/#prod-CallExpression
+ * @see https://tc39.es/ecma262/#prod-OptionalExpression
+ */
 export const writeCallExpression: Writer<CallExpression> = (node, write) => {
   write(node.callee);
+
+  if (node.optional) {
+    write('?.');
+  }
+
   write('(');
 
   for (let i = 0; i < node.arguments.length; i++) {
     if (i >= 1) {
-      write(', ');
+      write(',');
     }
 
     write(node.arguments[i]!);
