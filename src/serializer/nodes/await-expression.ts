c2421cbc1b46a5c6d70e791569c898b57c33dd04
Robin Blomberg <hello@robinblomberg.se>
2023-04-20 16:54:58 +0200

    feat(serializer): support AwaitExpression

--- src/serializer/nodes/await-expression.ts
+++ src/serializer/nodes/await-expression.ts
@@ -1,7 +1,15 @@
 import { AwaitExpression } from '../../estree';
 import { Writer } from '../write';
 
-export const writeAwaitExpression: Writer<AwaitExpression> = (
-  node,
-  write,
-) => {};
+/**
+ * ```ecmarkup
+ * AwaitExpression[Yield] :
+ *   await UnaryExpression[?Yield, +Await]
+ * ```
+ *
+ * @see https://tc39.es/ecma262/#prod-AwaitExpression
+ */
+export const writeAwaitExpression: Writer<AwaitExpression> = (node, write) => {
+  write('await');
+  write(node.argument);
+};
