a821fbe7a81be27eb4d4325251a18d3443c54085
Robin Blomberg <hello@robinblomberg.se>
2023-04-20 16:57:06 +0200

    feat(serializer): add BinaryExpression spec

--- src/serializer/nodes/binary-expression.ts
+++ src/serializer/nodes/binary-expression.ts
@@ -1,6 +1,15 @@
 import { BinaryExpression } from '../../estree';
 import { Writer } from '../write';
 
+/**
+ * ```ecmarkup
+ * BitwiseORExpression[In, Yield, Await] :
+ *   BitwiseXORExpression[?In, ?Yield, ?Await]
+ *   BitwiseORExpression[?In, ?Yield, ?Await] | BitwiseXORExpression[?In, ?Yield, ?Await]
+ * ```
+ *
+ * @see https://tc39.es/ecma262/#prod-BitwiseORExpression
+ */
 export const writeBinaryExpression: Writer<BinaryExpression> = (
   node,
   write,
