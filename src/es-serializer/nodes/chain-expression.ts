ada83c87bddb3f8b4e4863bb97855b0c647a66f8
Robin Blomberg <hello@robinblomberg.se>
2023-04-23 16:09:25 +0200

    fix(es-serializer): fix lint error

--- src/es-serializer/nodes/chain-expression.ts
+++ src/es-serializer/nodes/chain-expression.ts
@@ -2,11 +2,11 @@ import { ChainExpression } from '../../es-ast';
 import { Writer } from '../serialize';
 
 /**
- * "Evaluation:
+ * Evaluation:
  *   - The `ChainExpression` node is evaluated to the result of the `expression` property's node.
  *   - If the `callee|object` property is evaluated to nullish and the `optional` property is
  *     `true`, then the node and ancestor nodes are skipped until the closest `ChainExpression`
- *     node, and the result of the `ChainExpression` node becomes `undefined`."
+ *     node, and the result of the `ChainExpression` node becomes `undefined`.
  *
  * @see https://github.com/estree/estree/blob/master/es2020.md#chainexpression
  */
