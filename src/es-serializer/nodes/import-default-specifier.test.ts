6323368c18fb9c92720562e2633bdf7a3016f0ff
Robin Blomberg <hello@robinblomberg.se>
2023-04-24 14:51:59 +0200

    feat(es-serializer): support ImportDefaultSpecifier

---
+++ src/es-serializer/nodes/import-default-specifier.test.ts
@@ -0,0 +1,25 @@
+import { expect, suite, test } from 'vitest';
+import { Identifier, ImportDefaultSpecifier } from '../../es-ast';
+import { serialize } from '../serialize';
+
+suite('ImportDefaultSpecifier', () => {
+  suite('ImportedBinding', () => {
+    suite('BindingIdentifier[~Yield, +Await]', () => {
+      test('Identifier', () => {
+        expect(serialize(ImportDefaultSpecifier(Identifier('a')))).toBe('a');
+      });
+
+      test('yield', () => {
+        expect(serialize(ImportDefaultSpecifier(Identifier('yield')))).toBe(
+          'yield',
+        );
+      });
+
+      test('await', () => {
+        expect(serialize(ImportDefaultSpecifier(Identifier('await')))).toBe(
+          'await',
+        );
+      });
+    });
+  });
+});
--- src/es-serializer/nodes/import-default-specifier.ts
+++ src/es-serializer/nodes/import-default-specifier.ts
@@ -1,7 +1,25 @@
 import { ImportDefaultSpecifier } from '../../es-ast';
 import { Writer } from '../serialize';
 
+/**
+ * ```ecmarkup
+ * ImportedDefaultBinding :
+ *   ImportedBinding
+ *
+ * ImportedBinding :
+ *   BindingIdentifier[~Yield, +Await]
+ *
+ * BindingIdentifier[Yield, Await] :
+ *   Identifier
+ *   yield
+ *   await
+ * ```
+ *
+ * @see https://tc39.es/ecma262/#prod-ImportedDefaultBinding
+ */
 export const writeImportDefaultSpecifier: Writer<ImportDefaultSpecifier> = (
   node,
   write,
-) => {};
+) => {
+  write(node.local);
+};
