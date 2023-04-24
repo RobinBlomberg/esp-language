d7e66d069c714f82a12a1f8f286f0d07328830de
Robin Blomberg <hello@robinblomberg.se>
2023-04-24 14:29:15 +0200

    feat(es-serializer): support UpdateExpression

---
+++ src/es-serializer/nodes/update-expression.test.ts
@@ -0,0 +1,41 @@
+import { expect, suite, test } from 'vitest';
+import { Identifier, UnaryExpression, UpdateExpression } from '../../es-ast';
+import { serialize } from '../serialize';
+
+suite('UpdateExpression', () => {
+  test('LeftHandSideExpression[?Yield, ?Await] [no LineTerminator here] ++', () => {
+    expect(serialize(UpdateExpression('++', Identifier('a'), false))).toBe(
+      'a++',
+    );
+  });
+
+  test('LeftHandSideExpression[?Yield, ?Await] [no LineTerminator here] --', () => {
+    expect(serialize(UpdateExpression('--', Identifier('a'), false))).toBe(
+      'a--',
+    );
+  });
+
+  test('++ UnaryExpression[?Yield, ?Await]', () => {
+    expect(
+      serialize(
+        UpdateExpression(
+          '++',
+          UnaryExpression('!', true, Identifier('a')),
+          true,
+        ),
+      ),
+    ).toBe('++!a');
+  });
+
+  test('-- UnaryExpression[?Yield, ?Await]', () => {
+    expect(
+      serialize(
+        UpdateExpression(
+          '--',
+          UnaryExpression('!', true, Identifier('a')),
+          true,
+        ),
+      ),
+    ).toBe('--!a');
+  });
+});
--- src/es-serializer/nodes/update-expression.ts
+++ src/es-serializer/nodes/update-expression.ts
@@ -1,6 +1,18 @@
 import { UpdateExpression } from '../../es-ast';
 import { Writer } from '../serialize';
 
+/**
+ * ```ecmarkup
+ * UpdateExpression[Yield, Await] :
+ *   LeftHandSideExpression[?Yield, ?Await]
+ *   LeftHandSideExpression[?Yield, ?Await] [no LineTerminator here] ++
+ *   LeftHandSideExpression[?Yield, ?Await] [no LineTerminator here] --
+ *   ++ UnaryExpression[?Yield, ?Await]
+ *   -- UnaryExpression[?Yield, ?Await]
+ * ```
+ *
+ * @see https://tc39.es/ecma262/#prod-UpdateExpression
+ */
 export const writeUpdateExpression: Writer<UpdateExpression> = (
   node,
   write,
