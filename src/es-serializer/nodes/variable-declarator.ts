513d0c2d4055ef06dd4ef0edfc71eb77d3f76e51
Robin Blomberg <hello@robinblomberg.se>
2023-04-24 17:00:08 +0200

    feat(es-serializer): support VariableDeclarator

--- src/es-serializer/nodes/variable-declarator.ts
+++ src/es-serializer/nodes/variable-declarator.ts
@@ -1,6 +1,15 @@
 import { VariableDeclarator } from '../../es-ast';
 import { Writer } from '../serialize';
 
+/**
+ * ```ecmarkup
+ * VariableDeclaration[In, Yield, Await] :
+ *   BindingIdentifier[?Yield, ?Await] Initializer[?In, ?Yield, ?Await]<opt>
+ *   BindingPattern[?Yield, ?Await] Initializer[?In, ?Yield, ?Await]
+ * ```
+ *
+ * @see https://tc39.es/ecma262/#prod-VariableDeclaration
+ */
 export const writeVariableDeclarator: Writer<VariableDeclarator> = (
   node,
   write,
