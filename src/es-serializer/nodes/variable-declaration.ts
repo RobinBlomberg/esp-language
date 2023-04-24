a899f5c1f6be009e71f0185f633998c047943571
Robin Blomberg <hello@robinblomberg.se>
2023-04-24 17:18:06 +0200

    feat(es-serializer): support VariableDeclaration

--- src/es-serializer/nodes/variable-declaration.ts
+++ src/es-serializer/nodes/variable-declaration.ts
@@ -1,6 +1,38 @@
 import { VariableDeclaration } from '../../es-ast';
 import { Writer } from '../serialize';
 
+/**
+ * ```ecmarkup
+ * VariableStatement[Yield, Await] :
+ *   var VariableDeclarationList[+In, ?Yield, ?Await] ;
+ *
+ * VariableDeclarationList[In, Yield, Await] :
+ *   VariableDeclaration[?In, ?Yield, ?Await]
+ *   VariableDeclarationList[?In, ?Yield, ?Await] , VariableDeclaration[?In, ?Yield, ?Await]
+ *
+ * VariableDeclaration[In, Yield, Await] :
+ *   BindingIdentifier[?Yield, ?Await] Initializer[?In, ?Yield, ?Await]<opt>
+ *   BindingPattern[?Yield, ?Await] Initializer[?In, ?Yield, ?Await]
+ *
+ * LexicalDeclaration[In, Yield, Await] :
+ *   LetOrConst BindingList[?In, ?Yield, ?Await] ;
+ *
+ * LetOrConst :
+ *   let
+ *   const
+ *
+ * BindingList[In, Yield, Await] :
+ *   LexicalBinding[?In, ?Yield, ?Await]
+ *   BindingList[?In, ?Yield, ?Await] , LexicalBinding[?In, ?Yield, ?Await]
+ *
+ * LexicalBinding[In, Yield, Await] :
+ *   BindingIdentifier[?Yield, ?Await] Initializer[?In, ?Yield, ?Await]<opt>
+ *   BindingPattern[?Yield, ?Await] Initializer[?In, ?Yield, ?Await]
+ * ```
+ *
+ * @see https://tc39.es/ecma262/#prod-VariableStatement
+ * @see https://tc39.es/ecma262/#prod-LexicalDeclaration
+ */
 export const writeVariableDeclaration: Writer<VariableDeclaration> = (
   node,
   write,
