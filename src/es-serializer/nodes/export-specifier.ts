c69dbe80186bf299ba8551b6201318dfc5a1c656
Robin Blomberg <hello@robinblomberg.se>
2023-04-24 16:57:04 +0200

    feat(es-serializer): support ExportNamedDeclaration

--- src/es-serializer/nodes/export-specifier.ts
+++ src/es-serializer/nodes/export-specifier.ts
@@ -1,7 +1,31 @@
-import { ExportSpecifier } from '../../es-ast';
+import { ExportSpecifier, NodeType } from '../../es-ast';
 import { Writer } from '../serialize';
 
-export const writeExportSpecifier: Writer<ExportSpecifier> = (
-  node,
-  write,
-) => {};
+/**
+ * ```ecmarkup
+ * ExportSpecifier :
+ *   ModuleExportName
+ *   ModuleExportName as ModuleExportName
+ *
+ * ModuleExportName :
+ *   IdentifierName
+ *   StringLiteral
+ * ```
+ *
+ * @see https://tc39.es/ecma262/#prod-ExportSpecifier
+ */
+export const writeExportSpecifier: Writer<ExportSpecifier> = (node, write) => {
+  write(node.local);
+
+  const local =
+    node.local.type === NodeType.Literal ? node.local.value : node.local.name;
+  const exported =
+    node.exported.type === NodeType.Literal
+      ? node.exported.value
+      : node.exported.name;
+
+  if (local !== exported) {
+    write('as');
+    write(node.exported);
+  }
+};
