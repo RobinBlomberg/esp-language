6a766f603f052419ea55f0e2adbca30c423d7141
Robin Blomberg <hello@robinblomberg.se>
2023-04-24 15:16:20 +0200

    feat(es-serializer): support ImportDeclaration

---
+++ src/es-serializer/nodes/import-declaration.test.ts
@@ -0,0 +1,106 @@
+import { expect, suite, test } from 'vitest';
+import {
+  Identifier,
+  ImportDeclaration,
+  ImportDefaultSpecifier,
+  ImportNamespaceSpecifier,
+  ImportSpecifier,
+  Literal,
+} from '../../es-ast';
+import { serialize } from '../serialize';
+
+suite('ImportDeclaration', () => {
+  suite('import ImportClause FromClause ;', () => {
+    suite('ImportedDefaultBinding', () => {
+      suite('ImportedBinding', () => {
+        test('BindingIdentifier[~Yield, +Await]', () => {
+          expect(
+            serialize(
+              ImportDeclaration(
+                [ImportDefaultSpecifier(Identifier('a'))],
+                Literal('b'),
+              ),
+            ),
+          ).toBe('import a from"b";');
+        });
+      });
+    });
+
+    suite('NameSpaceImport', () => {
+      test('* as ImportedBinding', () => {
+        expect(
+          serialize(
+            ImportDeclaration(
+              [ImportNamespaceSpecifier(Identifier('a'))],
+              Literal('b'),
+            ),
+          ),
+        ).toBe('import*as a from"b";');
+      });
+    });
+
+    suite('NamedImports', () => {
+      suite('{ ImportsList }', () => {
+        suite('ImportsList', () => {
+          test('ImportSpecifier', () => {
+            expect(
+              serialize(
+                ImportDeclaration(
+                  [ImportSpecifier(Identifier('a'), Identifier('a'))],
+                  Literal('b'),
+                ),
+              ),
+            ).toBe('import{a}from"b";');
+          });
+
+          test('ImportsList , ImportSpecifier', () => {
+            expect(
+              serialize(
+                ImportDeclaration(
+                  [
+                    ImportSpecifier(Identifier('a'), Identifier('b')),
+                    ImportSpecifier(Identifier('c'), Identifier('c')),
+                  ],
+                  Literal('d'),
+                ),
+              ),
+            ).toBe('import{a as b,c}from"d";');
+          });
+        });
+      });
+    });
+
+    test('ImportedDefaultBinding , NameSpaceImport', () => {
+      expect(
+        serialize(
+          ImportDeclaration(
+            [
+              ImportDefaultSpecifier(Identifier('a')),
+              ImportNamespaceSpecifier(Identifier('b')),
+            ],
+            Literal('c'),
+          ),
+        ),
+      ).toBe('import a,*as b from"c";');
+    });
+
+    test('ImportedDefaultBinding , NamedImports', () => {
+      expect(
+        serialize(
+          ImportDeclaration(
+            [
+              ImportDefaultSpecifier(Identifier('a')),
+              ImportSpecifier(Identifier('b'), Identifier('c')),
+              ImportSpecifier(Identifier('d'), Identifier('d')),
+            ],
+            Literal('e'),
+          ),
+        ),
+      ).toBe('import a,{b as c,d}from"e";');
+    });
+  });
+
+  test('import ModuleSpecifier ;', () => {
+    expect(serialize(ImportDeclaration([], Literal('b')))).toBe('import"b";');
+  });
+});
--- src/es-serializer/nodes/import-declaration.ts
+++ src/es-serializer/nodes/import-declaration.ts
@@ -1,7 +1,80 @@
-import { ImportDeclaration } from '../../es-ast';
+import { ImportDeclaration, NodeType } from '../../es-ast';
 import { Writer } from '../serialize';
 
+/**
+ * ```ecmarkup
+ * ImportDeclaration :
+ *   import ImportClause FromClause ;
+ *   import ModuleSpecifier ;
+ *
+ * ImportClause :
+ *   ImportedDefaultBinding
+ *   NameSpaceImport
+ *   NamedImports
+ *   ImportedDefaultBinding , NameSpaceImport
+ *   ImportedDefaultBinding , NamedImports
+ *
+ * ImportedDefaultBinding :
+ *   ImportedBinding
+ *
+ * NameSpaceImport :
+ *   * as ImportedBinding
+ *
+ * NamedImports :
+ *   ...
+ *   { ImportsList }
+ *   ...
+ *
+ * FromClause :
+ *   from ModuleSpecifier
+ *
+ * ImportsList :
+ *   ImportSpecifier
+ *   ImportsList , ImportSpecifier
+ *
+ * ModuleSpecifier :
+ *   StringLiteral
+ *
+ * ImportedBinding :
+ *   BindingIdentifier[~Yield, +Await]
+ * ```
+ *
+ * @see https://tc39.es/ecma262/#prod-ImportDeclaration
+ */
 export const writeImportDeclaration: Writer<ImportDeclaration> = (
   node,
   write,
-) => {};
+) => {
+  write('import');
+
+  if (node.specifiers.length >= 1) {
+    let hasImportSpecifierBeenWritten = false;
+
+    for (let i = 0; i < node.specifiers.length; i++) {
+      if (i >= 1) {
+        write(',');
+      }
+
+      const specifier = node.specifiers[i]!;
+
+      if (
+        specifier.type === NodeType.ImportSpecifier &&
+        !hasImportSpecifierBeenWritten
+      ) {
+        write('{');
+        hasImportSpecifierBeenWritten = true;
+      }
+
+      write(specifier);
+    }
+
+    if (hasImportSpecifierBeenWritten) {
+      write('}');
+    }
+
+    write('from');
+  }
+
+  write(node.source);
+  write(';');
+};
