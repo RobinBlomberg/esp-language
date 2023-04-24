4c176959e4e936d42ff842622a39199bb2bddd9f
Robin Blomberg <hello@robinblomberg.se>
2023-04-24 16:22:57 +0200

    feat(es-serializer): support ExportDefaultDeclaration

---
+++ src/es-serializer/nodes/export-default-declaration.test.ts
@@ -0,0 +1,47 @@
+import { expect, suite, test } from 'vitest';
+import {
+  AnonymousDefaultExportedClassDeclaration,
+  AnonymousDefaultExportedFunctionDeclaration,
+  BlockStatement,
+  ClassBody,
+  ExportDefaultDeclaration,
+  Identifier,
+} from '../../es-ast';
+import { serialize } from '../serialize';
+
+suite('ExportDefaultDeclaration', () => {
+  test('export default HoistableDeclaration[~Yield, +Await, +Default]', () => {
+    expect(
+      serialize(
+        ExportDefaultDeclaration(
+          AnonymousDefaultExportedFunctionDeclaration(
+            [],
+            BlockStatement([]),
+            false,
+            false,
+          ),
+        ),
+      ),
+    ).toBe('export default function(){}');
+  });
+
+  test('export default ClassDeclaration[~Yield, +Await, +Default]', () => {
+    expect(
+      serialize(
+        ExportDefaultDeclaration(
+          AnonymousDefaultExportedClassDeclaration(null, ClassBody([])),
+        ),
+      ),
+    ).toBe('export default class{}');
+  });
+
+  test(
+    'export default [lookahead ∉ { function, async [no LineTerminator here] function, class }] ' +
+      'AssignmentExpression[+In, ~Yield, +Await] ;',
+    () => {
+      expect(serialize(ExportDefaultDeclaration(Identifier('a')))).toBe(
+        'export default a;',
+      );
+    },
+  );
+});
--- src/es-serializer/nodes/export-default-declaration.ts
+++ src/es-serializer/nodes/export-default-declaration.ts
@@ -1,7 +1,30 @@
-import { ExportDefaultDeclaration } from '../../es-ast';
+import { ExportDefaultDeclaration, NodeType } from '../../es-ast';
 import { Writer } from '../serialize';
 
+/**
+ * ```ecmarkup
+ * ExportDeclaration :
+ *   ...
+ *   export default HoistableDeclaration[~Yield, +Await, +Default]
+ *   export default ClassDeclaration[~Yield, +Await, +Default]
+ *   export default [lookahead ∉ { function, async [no LineTerminator here] function, class }]
+ *     AssignmentExpression[+In, ~Yield, +Await] ;
+ * ```
+ *
+ * @see https://tc39.es/ecma262/#prod-ExportDeclaration
+ */
 export const writeExportDefaultDeclaration: Writer<ExportDefaultDeclaration> = (
   node,
   write,
-) => {};
+) => {
+  write('export');
+  write('default');
+  write(node.declaration);
+
+  if (
+    node.declaration.type !== NodeType.FunctionDeclaration &&
+    node.declaration.type !== NodeType.ClassDeclaration
+  ) {
+    write(';');
+  }
+};
