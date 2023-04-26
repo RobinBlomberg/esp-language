fc782e0096fa0e40efed0ff2f14db99cb59798f2
Robin Blomberg <hello@robinblomberg.se>
2023-04-26 13:12:29 +0200

    feat(esp-es-transformer): pass array as argument

--- src/esp-es-transformer/nodes/set-literal.ts
+++ src/esp-es-transformer/nodes/set-literal.ts
@@ -3,8 +3,7 @@ import { ESP } from '../../esp-parser';
 import { transformExpression } from './expression';
 
 export const transformSetLiteral = (node: ESP.SetLiteral) => {
-  return ES.NewExpression(
-    ES.Identifier('Set'),
-    node.values.map(transformExpression),
-  );
+  return ES.NewExpression(ES.Identifier('Set'), [
+    ES.ArrayExpression(node.values.map(transformExpression)),
+  ]);
 };
