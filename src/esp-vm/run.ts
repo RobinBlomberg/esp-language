33913140004a0e77feb755d1e802fc4ba1db13ea
Robin Blomberg <hello@robinblomberg.se>
2023-04-26 13:18:50 +0200

    feat(esp-vm): improve logging

--- src/esp-vm/run.ts
+++ src/esp-vm/run.ts
@@ -4,16 +4,46 @@ import { transformScript } from '../esp-es-transformer';
 import { abrupt } from '../esp-lexer';
 import { parseScript } from '../esp-parser';
 
+const logSyntaxError = (script: string, errorIndex: number) => {
+  let line = 1;
+  let column = 1;
+  let lineStart = 0;
+  let lineEnd = script.length;
+
+  for (let i = 0; i < script.length; i++) {
+    if (script[i] === '\n' || (script[i] === '\r' && script[i + 1] !== '\n')) {
+      if (i > errorIndex) {
+        lineEnd = i;
+        break;
+      }
+
+      line++;
+      column = 1;
+      lineStart = i + 1;
+    } else if (i < errorIndex) {
+      column++;
+    }
+  }
+
+  const character =
+    errorIndex < script.length
+      ? `character ${JSON.stringify(script[errorIndex])}`
+      : 'end of file';
+
+  console.error(script.slice(lineStart, lineEnd));
+  console.error(`${' '.repeat(errorIndex - lineStart)}^`);
+  console.error();
+  console.error(`\x1b[31m(${line}:${column}) Unexpected ${character}\x1b[0m`);
+  console.error();
+};
+
 export const run = (script: string) => {
+  console.clear();
+
   const parseResult = parseScript(script, 0);
 
   if (abrupt(parseResult)) {
-    const index = parseResult.start;
-    const character =
-      index < script.length
-        ? `character ${JSON.stringify(script[index])}`
-        : 'end of file';
-    console.error(`SyntaxError: Unexpected ${character} at index ${index}`);
+    logSyntaxError(script, parseResult.start);
     process.exit(1);
   }
 
