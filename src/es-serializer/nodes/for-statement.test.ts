af5a62782815d9981a8e15eec6546cf4d1645c00
Robin Blomberg <hello@robinblomberg.se>
2023-04-26 07:58:09 +0200

    feat(es-serializer): support ForStatement

---
+++ src/es-serializer/nodes/for-statement.test.ts
@@ -0,0 +1,75 @@
+import { expect, suite, test } from 'vitest';
+import {
+  ExpressionStatement,
+  ForStatement,
+  Identifier,
+  VariableDeclaration,
+  VariableDeclarator,
+} from '../../es-ast';
+import { serialize } from '../serialize';
+
+suite('ForStatement', () => {
+  test(
+    'for ( [lookahead ≠ let [] Expression[~In, ?Yield, ?Await]<opt> ; ' +
+      'Expression[+In, ?Yield, ?Await]<opt> ; Expression[+In, ?Yield, ?Await]<opt> ) ' +
+      'Statement[?Yield, ?Await, ?Return]',
+    () => {
+      expect(
+        serialize(
+          ForStatement(
+            Identifier('a'),
+            Identifier('b'),
+            Identifier('c'),
+            ExpressionStatement(Identifier('d')),
+          ),
+        ),
+      ).toBe('for(a;b;c)d;');
+    },
+  );
+
+  test(
+    'for ( var VariableDeclarationList[~In, ?Yield, ?Await] ; Expression[+In, ?Yield, ?Await]<opt> ; ' +
+      'Expression[+In, ?Yield, ?Await]<opt> ) Statement[?Yield, ?Await, ?Return]',
+    () => {
+      expect(
+        serialize(
+          ForStatement(
+            VariableDeclaration(
+              [
+                VariableDeclarator(Identifier('a'), null),
+                VariableDeclarator(Identifier('b'), Identifier('c')),
+              ],
+              'var',
+            ),
+            Identifier('d'),
+            Identifier('e'),
+            ExpressionStatement(Identifier('f')),
+          ),
+        ),
+      ).toBe('for(var a,b=c;d;e)f;');
+    },
+  );
+
+  test(
+    'for ( LexicalDeclaration[~In, ?Yield, ?Await] Expression[+In, ?Yield, ?Await]<opt> ; ' +
+      'Expression[+In, ?Yield, ?Await]<opt> ) Statement[?Yield, ?Await, ?Return]',
+    () => {
+      expect(
+        serialize(
+          ForStatement(
+            VariableDeclaration(
+              [
+                VariableDeclarator(Identifier('a'), null),
+                VariableDeclarator(Identifier('b'), Identifier('c')),
+              ],
+              'let',
+            ),
+            Identifier('d'),
+            Identifier('e'),
+            ExpressionStatement(Identifier('f')),
+          ),
+        ),
+      ).toBe('for(let a,b=c;d;e)f;');
+    },
+  );
+});
