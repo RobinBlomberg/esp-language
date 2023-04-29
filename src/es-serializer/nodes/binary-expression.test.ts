2071d5224f88dbc94f49b80c810219dccc6bb0a4
Robin Blomberg <hello@robinblomberg.se>
2023-04-29 14:44:34 +0200

    refactor(es-serializer): remove test-utils file

--- src/es-serializer/nodes/binary-expression.test.ts
+++ src/es-serializer/nodes/binary-expression.test.ts
@@ -1,114 +1,78 @@
-import { expect, it, suite } from 'vitest';
-import { BinaryExpression, Identifier, LogicalExpression } from '../../es-ast';
+import { expect, it, suite, test } from 'vitest';
+import {
+  BinaryExpression,
+  BinaryOperator,
+  Identifier,
+  LogicalExpression,
+  PrivateIdentifier,
+} from '../../es-ast';
 import { serialize } from '../serialize';
-import { testBinaryExpression } from '../test-utils';
 
 suite('BinaryExpression', () => {
-  testBinaryExpression(
-    'BitwiseORExpression[?In, ?Yield, ?Await] | BitwiseXORExpression[?In, ?Yield, ?Await]',
-    '|',
-  );
+  const tests = {
+    '|': 'BitwiseORExpression[?In, ?Yield, ?Await] | BitwiseXORExpression[?In, ?Yield, ?Await]',
+    '^': 'BitwiseXORExpression[?In, ?Yield, ?Await] ^ BitwiseANDExpression[?In, ?Yield, ?Await]',
+    '&': 'BitwiseANDExpression[?In, ?Yield, ?Await] & EqualityExpression[?In, ?Yield, ?Await]',
+    '==': 'EqualityExpression[?In, ?Yield, ?Await] == RelationalExpression[?In, ?Yield, ?Await]',
+    '!=': 'EqualityExpression[?In, ?Yield, ?Await] != RelationalExpression[?In, ?Yield, ?Await]',
+    '===':
+      'EqualityExpression[?In, ?Yield, ?Await] === RelationalExpression[?In, ?Yield, ?Await]',
+    '!==':
+      'EqualityExpression[?In, ?Yield, ?Await] !== RelationalExpression[?In, ?Yield, ?Await]',
+    '<': 'RelationalExpression[?In, ?Yield, ?Await] < ShiftExpression[?Yield, ?Await]',
+    '>': 'RelationalExpression[?In, ?Yield, ?Await] > ShiftExpression[?Yield, ?Await]',
+    '<=': 'RelationalExpression[?In, ?Yield, ?Await] <= ShiftExpression[?Yield, ?Await]',
+    '>=': 'RelationalExpression[?In, ?Yield, ?Await] >= ShiftExpression[?Yield, ?Await]',
+    instanceof:
+      'RelationalExpression[?In, ?Yield, ?Await] instanceof ShiftExpression[?Yield, ?Await]',
+    in: '[+In] RelationalExpression[+In, ?Yield, ?Await] in ShiftExpression[?Yield, ?Await]',
+    '<<': 'ShiftExpression[?Yield, ?Await] << AdditiveExpression[?Yield, ?Await]',
+    '>>': 'ShiftExpression[?Yield, ?Await] >> AdditiveExpression[?Yield, ?Await]',
+    '>>>':
+      'ShiftExpression[?Yield, ?Await] >>> AdditiveExpression[?Yield, ?Await]',
+    '+': 'AdditiveExpression[?Yield, ?Await] + MultiplicativeExpression[?Yield, ?Await]',
+    '-': 'AdditiveExpression[?Yield, ?Await] - MultiplicativeExpression[?Yield, ?Await]',
+    '**': 'UpdateExpression[?Yield, ?Await] ** ExponentiationExpression[?Yield, ?Await]',
+  };
 
-  testBinaryExpression(
-    'BitwiseXORExpression[?In, ?Yield, ?Await] ^ BitwiseANDExpression[?In, ?Yield, ?Await]',
-    '^',
-  );
+  for (const [operator, name] of Object.entries(tests)) {
+    const space = /[a-z]/.test(operator) ? ' ' : '';
 
-  testBinaryExpression(
-    'BitwiseANDExpression[?In, ?Yield, ?Await] & EqualityExpression[?In, ?Yield, ?Await]',
-    '&',
-  );
+    test(name, () => {
+      expect(
+        serialize(
+          BinaryExpression(
+            operator as BinaryOperator,
+            Identifier('a'),
+            Identifier('b'),
+          ),
+        ),
+      ).toBe(`a${space}${operator}${space}b`);
+    });
+  }
 
-  testBinaryExpression(
-    'EqualityExpression[?In, ?Yield, ?Await] == RelationalExpression[?In, ?Yield, ?Await]',
-    '==',
-  );
+  test('[+In] PrivateIdentifier in ShiftExpression[?Yield, ?Await]', () => {
+    expect(
+      serialize(
+        BinaryExpression('in', PrivateIdentifier('a'), Identifier('b')),
+      ),
+    ).toBe('#a in b');
+  });
 
-  testBinaryExpression(
-    'EqualityExpression[?In, ?Yield, ?Await] != RelationalExpression[?In, ?Yield, ?Await]',
-    '!=',
-  );
-
-  testBinaryExpression(
-    'EqualityExpression[?In, ?Yield, ?Await] === RelationalExpression[?In, ?Yield, ?Await]',
-    '===',
-  );
-
-  testBinaryExpression(
-    'EqualityExpression[?In, ?Yield, ?Await] !== RelationalExpression[?In, ?Yield, ?Await]',
-    '!==',
-  );
-
-  testBinaryExpression(
-    'RelationalExpression[?In, ?Yield, ?Await] < ShiftExpression[?Yield, ?Await]',
-    '<',
-  );
-
-  testBinaryExpression(
-    'RelationalExpression[?In, ?Yield, ?Await] > ShiftExpression[?Yield, ?Await]',
-    '>',
-  );
-
-  testBinaryExpression(
-    'RelationalExpression[?In, ?Yield, ?Await] <= ShiftExpression[?Yield, ?Await]',
-    '<=',
-  );
-
-  testBinaryExpression(
-    'RelationalExpression[?In, ?Yield, ?Await] >= ShiftExpression[?Yield, ?Await]',
-    '>=',
-  );
-
-  testBinaryExpression(
-    'RelationalExpression[?In, ?Yield, ?Await] instanceof ShiftExpression[?Yield, ?Await]',
-    'instanceof',
-  );
-
-  testBinaryExpression(
-    '[+In] RelationalExpression[+In, ?Yield, ?Await] in ShiftExpression[?Yield, ?Await]',
-    'in',
-  );
-
-  testBinaryExpression(
-    '[+In] PrivateIdentifier in ShiftExpression[?Yield, ?Await]',
-    'in',
-    true,
-  );
-
-  testBinaryExpression(
-    'ShiftExpression[?Yield, ?Await] << AdditiveExpression[?Yield, ?Await]',
-    '<<',
-  );
-
-  testBinaryExpression(
-    'ShiftExpression[?Yield, ?Await] >> AdditiveExpression[?Yield, ?Await]',
-    '>>',
-  );
-
-  testBinaryExpression(
-    'ShiftExpression[?Yield, ?Await] >>> AdditiveExpression[?Yield, ?Await]',
-    '>>>',
-  );
-
-  testBinaryExpression(
-    'AdditiveExpression[?Yield, ?Await] + MultiplicativeExpression[?Yield, ?Await]',
-    '+',
-  );
-
-  testBinaryExpression(
-    'AdditiveExpression[?Yield, ?Await] - MultiplicativeExpression[?Yield, ?Await]',
-    '-',
-  );
-
-  testBinaryExpression(
+  suite(
     'MultiplicativeExpression[?Yield, ?Await] MultiplicativeOperator ' +
       'ExponentiationExpression[?Yield, ?Await]',
-    ['*', '/', '%'],
-  );
-
-  testBinaryExpression(
-    'UpdateExpression[?Yield, ?Await] ** ExponentiationExpression[?Yield, ?Await]',
-    '**',
+    () => {
+      for (const operator of ['*', '/', '%'] as const) {
+        test(operator, () => {
+          expect(
+            serialize(
+              BinaryExpression(operator, Identifier('a'), Identifier('b')),
+            ),
+          ).toBe(`a${operator}b`);
+        });
+      }
+    },
   );
 
   it('should parenthesize when needed', () => {
