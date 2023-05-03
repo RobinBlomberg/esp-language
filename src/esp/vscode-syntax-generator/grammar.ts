b31a2d52ae63f40c68c9d4acd4b64429cfd3d68f
Robin Blomberg <hello@robinblomberg.se>
2023-05-03 20:39:24 +0200

    refactor(vscode-syntax-generator): prefer regexes

--- src/esp/vscode-syntax-generator/grammar.ts
+++ src/esp/vscode-syntax-generator/grammar.ts
@@ -1,10 +1,73 @@
+import { escapeRegExpString } from '../../utils/escape-regexp-string';
+import { Name } from './names';
 import {
-  constant,
-  identifier,
-  include,
-  stringLiteral,
-} from './pattern-factory';
-import { Language } from './types';
+  ConstantKeywordNames,
+  ControlKeywordNames,
+  OperatorNames,
+  PunctuationNames,
+} from './semantic-names';
+import { Captures, Language, Pattern } from './types';
+
+const constant = (): Pattern => {
+  const nameMatchesMap: { [K in Name]?: string[] } = {};
+
+  for (const [match, name] of [
+    ...Object.entries(ConstantKeywordNames),
+    ...Object.entries(ControlKeywordNames),
+    ...Object.entries(OperatorNames),
+    ...Object.entries(PunctuationNames),
+  ]) {
+    if (name) {
+      (nameMatchesMap[name] ??= []).push(match);
+    }
+  }
+
+  const captures: Captures = {};
+  let index = 1;
+  let match = '';
+
+  for (const [name, matches] of Object.entries(nameMatchesMap)) {
+    if (index >= 2) {
+      match += '|';
+    }
+
+    match += `(${matches
+      .map((m) =>
+        /^[a-zA-Z$_][a-zA-Z0-9$_]*$/.test(m)
+          ? `\\b${escapeRegExpString(m)}\\b`
+          : escapeRegExpString(m),
+      )
+      .join('|')})`;
+    captures[index] = { name };
+    index++;
+  }
+
+  return { match, captures };
+};
+
+const identifier = () => {
+  return /(?<![a-zA-Z0-9$_])[a-zA-Z$_][a-zA-Z0-9$_]*(?![a-zA-Z0-9$_])/.source;
+};
+
+const include = (id: string) => {
+  return {
+    include: `#${id}` as const,
+  };
+};
+
+const stringLiteral = (
+  type: 'double' | 'single',
+  quoteChar: string,
+): Pattern => {
+  return {
+    name: `string.quoted.${type}.esp`,
+    begin: quoteChar,
+    beginCaptures: { 0: { name: 'punctuation.definition.string.begin.esp' } },
+    end: quoteChar,
+    endCaptures: { 0: { name: 'punctuation.definition.string.end.esp' } },
+    patterns: [include('StringEscape')],
+  };
+};
 
 export const language: Language = {
   name: 'ESP',
@@ -21,7 +84,7 @@ export const language: Language = {
       patterns: [
         {
           name: 'entity.name.function.esp',
-          match: identifier() + /(?=\s*(?:\(|(?:=\s*)?\(*:\s*\())/.source,
+          match: `${identifier()}${/(?=\s*(?:\(|(?:=\s*)?\(*:\s*\())/.source}`,
         },
       ],
     },
@@ -29,21 +92,15 @@ export const language: Language = {
       patterns: [
         {
           name: 'comment.block.esp',
-          begin: '/\\*',
-          beginCaptures: {
-            0: { name: 'punctuation.definition.comment.esp' },
-          },
-          end: '\\*/',
-          endCaptures: {
-            0: { name: 'punctuation.definition.comment.esp' },
-          },
+          begin: /\/\*/.source,
+          beginCaptures: { 0: { name: 'punctuation.definition.comment.esp' } },
+          end: /\*\//.source,
+          endCaptures: { 0: { name: 'punctuation.definition.comment.esp' } },
         },
         {
-          begin: '//',
-          beginCaptures: {
-            0: { name: 'comment.line.double-slash.esp' },
-          },
-          end: '(?=$)',
+          begin: /\//.source,
+          beginCaptures: { 0: { name: 'comment.line.double-slash.esp' } },
+          end: /(?=$)/.source,
           contentName: 'comment.line.double-slash.esp',
         },
       ],
@@ -66,7 +123,7 @@ export const language: Language = {
       patterns: [
         {
           name: 'constant.numeric.decimal.esp',
-          match: '\\b(?:0|[1-9][0-9]*)(?:\\.[0-9]*)?\\b',
+          match: /\b(?:0|[1-9][0-9]*)(?:\.[0-9]*)?\b/.source,
         },
       ],
     },
